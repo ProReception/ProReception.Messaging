b683e48e5ef703c7fe20468636063f03982f05d8
Søren Pedersen <sh.pedersen@gmail.com>
2023-05-29 11:30:44 +0200

    https://trello.com/c/75HnSoaq Add tests for testing the VisitPeriod, when the ClientSiteLocale has been set

--- UnitTests/VisitorArrivalMessageTests.cs
+++ UnitTests/VisitorArrivalMessageTests.cs
@@ -10,10 +10,14 @@
     {
         public static IEnumerable<object[]> GetVisitPeriodTestData()
         {
-            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2020-04-12"), "12 April, 2020" };
-            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2020-04-13"), "12 - 13 April, 2020" };
-            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2020-05-09"), "12 April - 9 May, 2020" };
-            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2021-01-05"), "12 April, 2020 - 5 January, 2021" };
+            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2020-04-12"), null, "12 April, 2020" };
+            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2020-04-13"), null, "12 - 13 April, 2020" };
+            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2020-05-09"), null, "12 April - 9 May, 2020" };
+            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2021-01-05"), null, "12 April, 2020 - 5 January, 2021" };
+            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2020-04-12"), "da-DK", "12 april, 2020" };
+            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2020-04-13"), "da-DK", "12 - 13 april, 2020" };
+            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2020-05-09"), "da-DK", "12 april - 9 maj, 2020" };
+            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2021-01-05"), "da-DK", "12 april, 2020 - 5 januar, 2021" };
         }
 
         public static IEnumerable<object[]> GetVisitorNameTestData()
@@ -24,12 +28,13 @@
 
         [Theory]
         [MemberData(nameof(GetVisitPeriodTestData))]
-        public void TestVisitPeriod(DateTime checkinTime, DateTime expectedCheckout, string expectedResult)
+        public void TestVisitPeriod(DateTime checkinTime, DateTime expectedCheckout, string clientSiteLocale, string expectedResult)
         {
             var message = new VisitorArrivalMessage
             {
                 CheckinTime = checkinTime,
-                ExpectedCheckout = expectedCheckout
+                ExpectedCheckout = expectedCheckout,
+                ClientSiteLocale = clientSiteLocale
             };
 
             message.VisitPeriod.ShouldBe(expectedResult);
