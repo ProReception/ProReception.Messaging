322a8d3a0f2909ce2232a9b9f4a110f309dab538
Søren Pedersen <sh.pedersen@gmail.com>
2026-02-18 15:58:49 +0100

    Replace `new object[]` with shorthand array syntax in unit test data.

--- UnitTests/VisitorArrivalMessageTests.cs
+++ UnitTests/VisitorArrivalMessageTests.cs
@@ -12,20 +12,20 @@
     {
         public static IEnumerable<object[]> GetVisitPeriodTestData()
         {
-            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2020-04-12"), null, "12 April, 2020" };
-            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2020-04-13"), null, "12 - 13 April, 2020" };
-            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2020-05-09"), null, "12 April - 9 May, 2020" };
-            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2021-01-05"), null, "12 April, 2020 - 5 January, 2021" };
-            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2020-04-12"), "da-DK", "12 april, 2020" };
-            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2020-04-13"), "da-DK", "12 - 13 april, 2020" };
-            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2020-05-09"), "da-DK", "12 april - 9 maj, 2020" };
-            yield return new object[] { DateTime.Parse("2020-04-12"), DateTime.Parse("2021-01-05"), "da-DK", "12 april, 2020 - 5 januar, 2021" };
+            yield return [DateTime.Parse("2020-04-12"), DateTime.Parse("2020-04-12"), null, "12 April, 2020"];
+            yield return [DateTime.Parse("2020-04-12"), DateTime.Parse("2020-04-13"), null, "12 - 13 April, 2020"];
+            yield return [DateTime.Parse("2020-04-12"), DateTime.Parse("2020-05-09"), null, "12 April - 9 May, 2020"];
+            yield return [DateTime.Parse("2020-04-12"), DateTime.Parse("2021-01-05"), null, "12 April, 2020 - 5 January, 2021"];
+            yield return [DateTime.Parse("2020-04-12"), DateTime.Parse("2020-04-12"), "da-DK", "12 april, 2020"];
+            yield return [DateTime.Parse("2020-04-12"), DateTime.Parse("2020-04-13"), "da-DK", "12 - 13 april, 2020"];
+            yield return [DateTime.Parse("2020-04-12"), DateTime.Parse("2020-05-09"), "da-DK", "12 april - 9 maj, 2020"];
+            yield return [DateTime.Parse("2020-04-12"), DateTime.Parse("2021-01-05"), "da-DK", "12 april, 2020 - 5 januar, 2021"];
         }
 
         public static IEnumerable<object[]> GetVisitorNameTestData()
         {
-            yield return new object[] { "Test Testesen", "Test", "Testesen" };
-            yield return new object[] { "Test Testesen Hansen", "Test", "Testesen Hansen" };
+            yield return ["Test Testesen", "Test", "Testesen"];
+            yield return ["Test Testesen Hansen", "Test", "Testesen Hansen"];
         }
 
         [Theory]
